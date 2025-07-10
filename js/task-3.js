7783407bf43fa59012dccec6d325c23cd52fb113
tha_scarfaze <thascarfaze@gmail.com>
2025-07-10 04:16:36 +0400

    update hw

--- js/task-3.js
+++
@@ -1,9 +0,0 @@
-const refs = {
-  nameInput: document.querySelector("#name-input"),
-  nameOutput: document.querySelector("#name-output"),
-};
-
-refs.nameInput.addEventListener("input", (event) => {
-  const value = event.currentTarget.value.trim();
-  refs.nameOutput.textContent = value || "Anonymous";
-});
